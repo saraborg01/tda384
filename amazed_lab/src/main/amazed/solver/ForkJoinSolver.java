9e5802eb4ae0624cc4d29c0691bfe52e69a5c9f6
saraborg01 <saraborg@LAPTOP-S1URB25R.localdomain>
2023-03-08 09:19:05 +0100

    lab 3 done

--- amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * <code>ForkJoinSolver</code> implements a solver for
@@ -22,6 +23,11 @@ import java.util.concurrent.ConcurrentSkipListSet;
 public class ForkJoinSolver
     extends SequentialSolver
 {
+    private static ConcurrentSkipListSet<Integer> concurrentVisited = new ConcurrentSkipListSet<>();
+    private List<ForkJoinSolver> childSolvers = new ArrayList<>();
+    private static AtomicBoolean finished = new AtomicBoolean();
+    private static Maze staticMaze;
+
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
@@ -47,8 +53,21 @@ public class ForkJoinSolver
     public ForkJoinSolver(Maze maze, int forkAfter)
     {
         this(maze);
+        staticMaze = maze;
         this.forkAfter = forkAfter;
     }
+
+    /**
+     * A constructor used for forking.
+     * Creates a child solver that searches in staticMaze from the
+     * start node to a goal. ForkAfter is ignored.
+     *
+     * @param start       the start node for this child thread.
+     */
+    public ForkJoinSolver( int start){
+		this(staticMaze);
+		this.start=start;
+	}
 
     /**
      * Searches for and returns the path, as a list of node
@@ -67,8 +86,101 @@ public class ForkJoinSolver
         return parallelSearch();
     }
 
+    /**
+     * Steps through the maze.
+     * Forks when more than one path is available.
+     *
+     * @return a list containing the path from start to goal, or null if no goal was found.
+     */
     private List<Integer> parallelSearch()
     {
+        // initiates a player on the maze on the start node
+        int player = staticMaze.newPlayer(start);
+
+        // start with starting node
+        frontier.push(start);
+
+        // explore all nodes in frontier or until goal has been found
+        while(!frontier.empty() && !finished.get()){
+            
+            int current = frontier.pop();
+
+            // if current node hasn't been explored, or if node is the starting node
+            // (already checked for the starting node of new solvers)
+            if (concurrentVisited.add(current) || current==start) {
+                
+                staticMaze.move(player, current);
+
+                // if node is the goal
+                if (maze.hasGoal(current)) {
+                    // signals all ForkJoinSolvers that goal has been found
+                    finished.compareAndSet(false, true);
+                    // reconstructs and returns path taken.
+                    return pathFromTo(start, current);
+                }
+
+                // checks all adjacent neigbors to the current node
+                for (int nb: staticMaze.neighbors(current)){
+
+                    // if neigbor hasn't been visited, the neighbor
+                    // can then be reached from current node.
+                    // And we add them to the frontier.
+                    if (!concurrentVisited.contains(nb)){
+                        predecessor.put(nb, current);
+                        frontier.push(nb);
+                    }
+                }
+            }
+
+            // No need to spawn child if there is only one path to take.
+            // Otherwise spawn a child solver for each adjacent path.
+            if(frontier.size() > 1){
+                while(!frontier.isEmpty()) {
+                    int neighbor = frontier.pop();
+                    spawnChild(neighbor);
+                }
+            }
+            
+        }
+
+        // reached a dead end or no more nodes to explore or another solver has signaled 
+        // that the goal has been found.
+        return joinChildren();
+    }
+
+    /**
+     * Spawns a child solver and forks them.
+     * Only spawns if the starting node has not been visited yet.
+     *
+     * @param nb - The neighbor node that the child will spawn on
+     */
+    private void spawnChild(int nb) {
+        if( concurrentVisited.add(nb)) {
+            ForkJoinSolver child = new ForkJoinSolver(nb);
+            childSolvers.add(child);
+            child.fork();
+        }
+    }
+
+    /**
+     * Solver is done with their tasks. Their frontier is empty or
+     * another ForkJoinSolver has signaled that they found the goal.
+     * It iterates through their children and joins, to see if any of them 
+     * has found the goal.
+     * The reconstructed path is a joined path from each child that has contributed 
+     * to have found the goal.
+     * @return - result if the goal was found. Otherwise null.
+     */
+    private List<Integer> joinChildren() {
+        for (ForkJoinSolver solver: childSolvers) {
+            List<Integer> result = solver.join();
+            if(result!=null) {
+                List<Integer> path = pathFromTo(start, predecessor.get(solver.start));
+                path.addAll(result);
+                return path;
+            }
+        }
         return null;
     }
 }
+
